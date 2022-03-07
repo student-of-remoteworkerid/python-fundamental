c7b05839fbeff3c6da4a755937878f3f91aa48be
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-03-07 08:42:25 +0800

    Initial commit and material sequential

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+Semua sintaksis dasar bahasa pemrograman terdiri dari:
+1. Sekuensial : langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+'''
+
+# Sekuensial
+print('Ibu berkata, "Pergi ke toko"')
+# print("Ibu berkata, \"Pergi ke toko\"")
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+print('Maka Budi berangkat ke toko')
+print('Budi mulai berbelanja')
